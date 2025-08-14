463c322e0a0f4284f9c7eb228f565214ffa8bbcf
casperdias <dias287804@gmail.com>
2025-08-14 14:34:11 +0700

    Refactor imports for PermissionForm, RoleForm, and UserForm components

--- resources/js/components/admin/permission/index.ts
+++ resources/js/components/admin/permission/index.ts
@@ -1,2 +1,3 @@
 export { default as AddPermission } from './AddPermission.vue';
 export { default as EditPermission } from './EditPermission.vue';
+export { default as PermissionForm } from './PermissionForm.vue';
--- resources/js/components/admin/role/index.ts
+++ resources/js/components/admin/role/index.ts
@@ -1,2 +1,3 @@
 export { default as AddRole } from './AddRole.vue';
 export { default as EditRole } from './EditRole.vue';
+export { default as RoleForm } from './RoleForm.vue';
