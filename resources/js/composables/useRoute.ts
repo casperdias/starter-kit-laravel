4157611f2fb048bb58117eee8b4e17761fc649b8
casperdias <dias287804@gmail.com>
2025-08-29 15:29:34 +0700

    remove

--- resources/js/composables/useRoute.ts
+++ resources/js/composables/useRoute.ts
@@ -1,7 +1,6 @@
 import { inject } from 'vue';
 import { route as ziggyRoute } from 'ziggy-js';
 
-// Export a function named 'route'
 export function useRoute() {
     return inject<typeof ziggyRoute>('route')!;
 }
