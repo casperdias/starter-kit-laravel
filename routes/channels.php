6b99e23abe783a169c650b265daea4d84d79d839
casperdias <dias287804@gmail.com>
2025-09-19 15:57:49 +0700

    FIX SJDNONSAD

--- routes/channels.php
+++ routes/channels.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+Broadcast::channel('App.Models.Auth.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
-});
-
-Broadcast::channel('push-notifications', function ($user) {
-    return $user != null;
 });
